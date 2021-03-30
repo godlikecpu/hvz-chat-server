877df86e9f2ff7a1008ccd380c8de742bb2ebab9
GodlikeCPU <dan.tomicic@gmail.com>
2021-03-30 17:14:01 +0200

    chore: CORS added

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const cors = require("cors");
+
+var io = require("socket.io")(http, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, jwt",
+      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+      "Access-Control-Allow-Credentials": true,
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
