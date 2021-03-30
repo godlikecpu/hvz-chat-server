3a6c0abf3b70909da309decee6897566b28a7853
GodlikeCPU <dan.tomicic@gmail.com>
2021-03-30 17:25:11 +0200

    CHORE: cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var io = require("socket.io")(http, {
   handlePreflightRequest: (req, res) => {
     const headers = {
       "Access-Control-Allow-Headers": "Content-Type, Authorization, jwt",
-      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+      "Access-Control-Allow-Origin": "*", //or the specific origin you want to give access to,
       "Access-Control-Allow-Credentials": true,
     };
     res.writeHead(200, headers);
