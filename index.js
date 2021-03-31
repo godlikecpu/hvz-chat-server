87659b9b5cc2c0e7dfeb1bef724911eb33558b9a
GodlikeCPU <dan.tomicic@gmail.com>
2021-03-31 09:40:32 +0200

    CHORE: HEROKU CORS

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(process.env.PORT | 3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log("listening on *:3000");
 });
